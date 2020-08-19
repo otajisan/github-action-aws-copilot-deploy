1fc70f82576736fe9e7795efe3ba7ab9614d192a
otajisan <taji.taji.otajisan@gmail.com>
2020-08-19 23:26:12 +0900

    :+1: add AWS credential settings

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM alpine:3.12.0
 
 RUN apk update
 RUN apk add curl
+RUN apk add docker
 
 # install AWS Copilot CLI
 RUN curl -Lo /usr/local/bin/copilot https://github.com/aws/copilot-cli/releases/download/v0.3.0/copilot-linux-amd64-v0.3.0
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh -l
 
+AWS_REGION=$1
+AWS_ACCESS_KEY=$2
+AWS_SECRET_KEY=$3
+APP=$4
+SVC=$5
+
+WORKDIR /copilot-ci
+
+# AWS profile and credential settings
+mkdir -p $HOME/.aws
+printf "[default]\nregion = $AWS_REGION\n" > $HOME/.aws/config
+printf "[default]\naws_access_key_id=$AWS_ACCESS_KEY\naws_secret_access_key=$AWS_SECRET_KEY\n" > $HOME/.aws/credentials
+
 copilot -h
+
+copilot init \
+  --app $APP \
+  --svc $SVC \
+  --svc-type 'Load Balanced Web Service' \
+  --dockerfile './Dockerfile' \
+  --deploy
